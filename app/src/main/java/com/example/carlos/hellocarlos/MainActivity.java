f53dfe88c881b18e606c9b3eb979fad4e363ef69
carlos_kim <carlos.jin568@gmail.com>
2018-12-27 07:49:14 +0800

    Modify MainActivity

--- app/src/main/java/com/example/carlos/hellocarlos/MainActivity.java
+++ app/src/main/java/com/example/carlos/hellocarlos/MainActivity.java
@@ -1,22 +1,31 @@
 package com.example.carlos.hellocarlos;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Button mBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        initData();
+        setListener();
     }
 
     private void initViews() {
-
+        mBtn = findViewById(R.id.button);
     }
 
     private void initData() {
@@ -25,7 +34,25 @@ public class MainActivity extends AppCompatActivity {
 
     private void setListener() {
         Log.e(TAG, "MainActivity setListener: ");
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.setData(Uri.parse("http://www.baidu.com"));
+//                startActivity(intent);
 
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Log.i(TAG, "onBackPressed: clicked.");
     }
 
     @Override
